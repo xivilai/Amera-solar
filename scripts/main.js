90cb1a629f3755601cba835480dae4b305cb43e5
Slava Logos <logos.slava@gmail.com>
2021-03-01 16:54:46 +0300

    Adding fancy animations

--- scripts/main.js
+++ scripts/main.js
@@ -64,24 +64,41 @@ function scrollTop(){
 window.addEventListener('scroll', scrollTop)
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
-// const sr = ScrollReveal({
-//     distance: '30px',
-//     duration: 1800,
-//     reset: true,
-// });
+const sr = ScrollReveal({
+    distance: '50px',
+    reset: true,
+    interval: 200,
+    duration: 1500
+})
 
-// sr.reveal(`.home__data, .home__img, 
-//            .decoration__data,
-//            .accessory__content,
-//            .footer__content`, {
-//     origin: 'top',
-//     interval: 200,
-// })
+sr.reveal(`.home__data, .home__img,
+     .economy__data, .installation__content,
+     .appointment__content, .footer__content`, {
+         duration: 1500,
+         origin: 'top',
+         scale: 0.85,
+     }
+);
 
-// sr.reveal(`.share__img, .send__content`, {
-//     origin: 'left'
-// })
+sr.reveal(`.installation__img`, {
+    distance: 0,
+    opacity: 0,
+    reset: false
+});
 
-// sr.reveal(`.share__data, .send__img`, {
-//     origin: 'right'
-// })
+sr.reveal(`.economy__img, .appointment__img`, {
+    scale: 0.5,
+    origin: 'bottom',
+    delay: 200
+});
+
+sr.reveal(`.appointment__content`, { 
+    distance: '200px',
+    origin: 'left'
+});
+sr.reveal(`.appointment__img`, { 
+    origin: 'right',
+    opacity: 0.3,
+    distance: '50px',
+    scale: 1
+});
