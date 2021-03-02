4bf2e6637e50896a7c6519e68bc5854ab1bb7670
Slava Logos <logos.slava@gmail.com>
2021-03-02 13:40:56 +0300

    - Remove section indicator - Align home section with logo

--- scripts/main.js
+++ scripts/main.js
@@ -27,26 +27,6 @@ const hideMenu = () => {
 
 hideMenu();
 
-/*=== Current visible section indicator === */
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
 /*==================== ADD SHADOW TO HEADER WHEN SCROLLED ====================*/ 
 function scrollHeader(){
     const nav = document.getElementById('header')
